dfac4c133f25adfe41d71497178aae5d5c064315
rubinapokhrel061 <pokhrelrubina2061.com>
2024-06-10 06:05:11 +0545

    change url

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-// const backendUrl = "https://lms-server-v1j9.onrender.com/";
-const backendUrl = "http://localhost:3000/";
+const backendUrl = "https://lms-server-v1j9.onrender.com/";
+// const backendUrl = "http://localhost:3000/";
 module.exports = backendUrl;
